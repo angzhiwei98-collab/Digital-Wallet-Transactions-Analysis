e13ac9bb7f2d47a630e24ef7713e4324044e9e54
angzhiwei98-collab <angzhiwei98@gmail.com>
2026-07-14 21:01:48 +0800

    Update 01_data_quality_check.sql

--- sql/01_data_quality_check.sql
+++ sql/01_data_quality_check.sql
@@ -1,9 +1,9 @@
+-- The dataset is ready for analysis when it was uploaded to Kaggle, yet the following simple data quality checks were conducted to confirm its readiness
 -- 01. To make sure the 5,000 transaction IDs are unique
 SELECT
   COUNT (DISTINCT transaction_id)
 FROM
   `project1-502206.digital_wallet.digital_wallet_transactions`
-
 
   
 -- 02. To check is there any negative value in numeric columns
