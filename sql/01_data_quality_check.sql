20cfd4cb5d2a8c0444532f334fbb2d3ac3123642
angzhiwei98-collab <angzhiwei98@gmail.com>
2026-07-14 20:53:34 +0800

    01_data_quality_check.sql

---
+++ sql/01_data_quality_check.sql
@@ -0,0 +1,16 @@
+-- 01. To make sure the 5,000 transaction IDs are unique
+SELECT
+  COUNT (DISTINCT transaction_id)
+FROM
+  `project1-502206.digital_wallet.digital_wallet_transactions`
+
+
+  
+-- 02. To check is there any negative value in numeric columns
+SELECT
+  COUNTIF(product_amount < 0),
+  COUNTIF(transaction_fee < 0),
+  COUNTIF(cashback < 0),
+  COUNTIF(loyalty_points < 0)
+FROM
+  `project1-502206.digital_wallet.digital_wallet_transactions`
